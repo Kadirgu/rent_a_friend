1ba802d069cd5ece896bac8c69c93f9b018b9a61
jenny-chacon <jpchacond1@gmail.com>
2022-11-22 12:18:03 +0100

    some changes create method friend_services controller

--- app/controllers/friend_services_controller.rb
+++ app/controllers/friend_services_controller.rb
@@ -1,4 +1,5 @@
 class FriendServicesController < ApplicationController
+
   def new
     @friend_service = FriendService.new
   end
@@ -9,6 +10,18 @@ class FriendServicesController < ApplicationController
 
   def create
     @friend_service = FriendService.new(friend_service_params)
+    @friend_service.user = current_user
+    if @friend_service.save
+      redirect_to friend_service_path(@friend_service)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def set_friend_service
+    @friend_service = FriendService.find(params[:id])
   end
 
   def friend_service_params
