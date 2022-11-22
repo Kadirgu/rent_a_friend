5c353ccc5d8f96745a2471b352db9b320e79585d
michellerodriguez <michellerzevada@gmail.com>
2022-11-22 14:46:27 +0100

    ff

--- app/controllers/friend_services_controller.rb
+++ app/controllers/friend_services_controller.rb
@@ -11,6 +11,20 @@ class FriendServicesController < ApplicationController
     @friend_service = FriendService.new(friend_service_params)
   end
 
+  def destroy
+    @user = user.find(params[:id])
+    @user.destroy
+    flash[:success] = "The user was successfully destroyed."
+    #redirect_to friends_service_path
+  end
+
+  def edit
+    user = User.find_by(id: params[:id])
+  if current_user == user
+    user.update()
+    flash[:success] = "The user was successfully updated."
+  end
+
   def friend_service_params
     params.require(:friend_service)
   end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,18 +19,18 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
   end
 
-  def destroy
-    @user = user.find(params[:id])
-    @user.destroy
-    flash[:success] = "The user was successfully destroyed."
-    #redirect_to friends_service_path
-  end
+  # def destroy 
+  #   @user = user.find(params[:id])
+  #   @user.destroy
+  #   flash[:success] = "The user was successfully destroyed."
+  #   #redirect_to friends_service_path
+  # end
 
-  def edit
-    user = User.find_by(id: params[:id])
-  if current_user == user
-    user.update()
-  end
+  # def edit
+  #   user = User.find_by(id: params[:id])
+  # if current_user == user
+  #   user.update()
+  # end
 
   # def update
   #   authorize @user
@@ -47,9 +47,6 @@ class UsersController < ApplicationController
     @user = current_user
   end
 end
-
-
-
 
 # This goes in the view for edit
 
