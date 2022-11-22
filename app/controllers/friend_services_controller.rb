a30bf514b72f78c5cd89d071459124d10ba4b4eb
Jasper Klose <klosejasper@gmail.com>
2022-11-22 14:10:20 +0100

    delete method done

--- app/controllers/friend_services_controller.rb
+++ app/controllers/friend_services_controller.rb
@@ -2,29 +2,25 @@ class FriendServicesController < ApplicationController
   def index
     @friend_services = FriendService.all
   end
-  
+
+  def show
+    @friend_service = FriendService.find(params[:id])
+  end
+
   def create
     @friend_service = FriendService.new(friend_service_params)
   end
 
   def destroy
-    @user = user.find(params[:id])
-    @user.destroy
+    @friend_service = FriendService.find(params[:id])
+    @friend_service.destroy
     flash[:success] = "The user was successfully destroyed."
-    #redirect_to friends_service_path
+    redirect_to friend_services_path
   end
 
-  def edit
-    user = User.find_by(id: params[:id])
-  if current_user == user
-    user.update()
-    flash[:success] = "The user was successfully updated."
-  end
+  private
 
   def friend_service_params
     params.require(:friend_service)
-
-  def show
-    @friend_service = FriendService.find(params[:id])
   end
 end
