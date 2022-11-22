49192dc153fce3d39c57bebce2ca3c355788149c
Jasper Klose <klosejasper@gmail.com>
2022-11-22 11:14:46 +0100

    view

--- app/controllers/friend_services_controller.rb
+++ app/controllers/friend_services_controller.rb
@@ -1,2 +1,17 @@
 class FriendServicesController < ApplicationController
+  def new
+    @friend_service = FriendService.new
+  end
+
+  def index
+    @friend_services = FriendService.all
+  end
+
+  def create
+    @friend_service = FriendService.new(friend_service_params)
+  end
+
+  def friend_service_params
+    params.require(:friend_service)
+  end
 end
