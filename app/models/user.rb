1caca2a4d87d4a10dcbfca384e56bdd9c95d06d9
jenny-chacon <jpchacond1@gmail.com>
2022-11-23 12:52:09 +0100

    create booking + show

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ApplicationRecord
     :recoverable, :rememberable, :validatable
 
   has_many :friend_services
+  has_many :bookings
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "pages#home"
+  root to: "friend_services#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :bookings
+  resources :bookings, only: :index
   resources :friend_services do
-    resources :bookings
+    resources :bookings, only: [:new, :create, :destroy]
   end
   resources :users
 
